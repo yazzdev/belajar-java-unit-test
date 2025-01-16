4c6e3e22973e4ee1b493c6af01c3c688f5a56884
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-16 22:12:33 +0700

    Menggunakan Assertions untuk membantu membuat unit test

--- src/main/java/dyaz/io/unit/test/Calculator.java
+++ src/main/java/dyaz/io/unit/test/Calculator.java
@@ -6,4 +6,12 @@ public class Calculator {
     return first + second;
   }
 
+  public Integer divide(Integer first, Integer second) {
+    if (second == 0) {
+      throw new IllegalArgumentException("Cannot divide by zero");
+    } else {
+      return first / second;
+    }
+  }
+
 }
--- src/test/java/dyaz/io/unit/test/CalculatorTest.java
+++ src/test/java/dyaz/io/unit/test/CalculatorTest.java
@@ -2,6 +2,8 @@ package dyaz.io.unit.test;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
 
   private Calculator calculator = new Calculator();
@@ -9,6 +11,22 @@ public class CalculatorTest {
   @Test
   public void testAddSuccess() {
     var result = calculator.add(10, 10);
+    // Using assertions
+    assertEquals(20, result);
+  }
+
+  @Test
+  public void testDivideSuccess(){
+    var result = calculator.divide(100, 10);
+    assertEquals(10, result);
+  }
+
+  @Test
+  public void testDivideFailed(){
+    // Using assertions for accept error value
+    assertThrows(IllegalArgumentException.class, () -> {
+      calculator.divide(10, 0);
+    } );
   }
 
 }
