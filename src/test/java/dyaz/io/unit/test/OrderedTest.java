0a2f55efbdc3e3dd683f036abb234bae33ed87b6
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-19 12:21:20 +0700

    Order Testing Method

---
+++ src/test/java/dyaz/io/unit/test/OrderedTest.java
@@ -0,0 +1,26 @@
+package dyaz.io.unit.test;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class OrderedTest {
+
+  @Test
+  @Order((1))
+  void test3() {
+  }
+
+  @Test
+  @Order((3))
+  void test1() {
+  }
+
+  @Test
+  @Order((2))
+  void test2() {
+  }
+
+}
