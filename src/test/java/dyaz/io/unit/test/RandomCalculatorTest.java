037b379bf2b8c414a9f2b0d832dd4a285c1dc067
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-22 20:04:46 +0700

    Repeated Test

--- src/test/java/dyaz/io/unit/test/RandomCalculatorTest.java
+++ src/test/java/dyaz/io/unit/test/RandomCalculatorTest.java
@@ -1,14 +1,46 @@
 package dyaz.io.unit.test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Random;
 
-public class RandomCalculatorTest extends AbstractCalculatorTest{
+public class RandomCalculatorTest extends AbstractCalculatorTest {
 
   @Test
   void testRandom(Random random) {
+    var a = random.nextInt();
+    var b = random.nextInt();
+
+    var result = calculator.add(a, b);
+    var expected = a + b;
+
+    Assertions.assertEquals(expected, result);
+  }
+
+  @DisplayName("Random test")
+  @RepeatedTest(
+          value = 10,
+          name = "{displayName} ke {currentRepetition} dari {totalRepetitions}"
+  )
+  void testRandomRepeat(Random random) {
+    var a = random.nextInt();
+    var b = random.nextInt();
+
+    var result = calculator.add(a, b);
+    var expected = a + b;
+
+    Assertions.assertEquals(expected, result);
+  }
+
+  @DisplayName("(Info) Random test")
+  @RepeatedTest(
+          value = 10,
+          name = "{displayName}"
+  )
+  void testRandomRepeatInfo(TestInfo testInfo, Random random, RepetitionInfo repetitionInfo) {
+    System.out.println(
+            testInfo.getDisplayName() + " ke " + repetitionInfo.getCurrentRepetition() + " dari " + repetitionInfo.getTotalRepetitions()
+    );
     var a = random.nextInt();
     var b = random.nextInt();
 
