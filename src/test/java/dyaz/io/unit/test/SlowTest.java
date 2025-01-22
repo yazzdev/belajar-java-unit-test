ef7ece3696fe9fe4bdf78d18b32a7780b3b7f2f0
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-22 20:32:25 +0700

    Parallel Execution Test

--- src/test/java/dyaz/io/unit/test/SlowTest.java
+++ src/test/java/dyaz/io/unit/test/SlowTest.java
@@ -2,14 +2,29 @@ package dyaz.io.unit.test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.TimeUnit;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class SlowTest {
 
   @Test
   @Timeout(value = 5, unit = TimeUnit.SECONDS)
-  void testSlow() throws InterruptedException {
+  void testSlow1() throws InterruptedException {
+    Thread.sleep(4_000);
+  }
+
+  @Test
+  @Timeout(value = 5, unit = TimeUnit.SECONDS)
+  void testSlow2() throws InterruptedException {
+    Thread.sleep(3_000);
+  }
+
+  @Test
+  @Timeout(value = 5, unit = TimeUnit.SECONDS)
+  void testSlow3() throws InterruptedException {
     Thread.sleep(5_000);
   }
 }
