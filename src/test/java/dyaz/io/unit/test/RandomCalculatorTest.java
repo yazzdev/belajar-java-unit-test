34fa93c8dfbcf2d3981ccd13aa9d53655fd403c6
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-22 20:16:00 +0700

    testing using parameter

--- src/test/java/dyaz/io/unit/test/RandomCalculatorTest.java
+++ src/test/java/dyaz/io/unit/test/RandomCalculatorTest.java
@@ -1,7 +1,11 @@
 package dyaz.io.unit.test;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
 import java.util.Random;
 
 public class RandomCalculatorTest extends AbstractCalculatorTest {
@@ -46,6 +50,30 @@ public class RandomCalculatorTest extends AbstractCalculatorTest {
 
     var result = calculator.add(a, b);
     var expected = a + b;
+
+    Assertions.assertEquals(expected, result);
+  }
+
+  @DisplayName("Test Calculator")
+  @ParameterizedTest(name = "{displayName} dengan parameter {0}")
+  @ValueSource(ints = {1, 3, 5, 2, 4})
+  void testWithParam(int value) {
+    var expected = value + value;
+    var result = calculator.add(value, value);
+
+    Assertions.assertEquals(expected, result);
+  }
+
+  public static List<Integer> parameterSource(){
+    return List.of(1,2,3);
+  }
+
+  @DisplayName("Test Calculator")
+  @ParameterizedTest(name = "{displayName} dengan parameter {0}")
+  @MethodSource({"parameterSource"})
+  void testWithMethodSrc(int value) {
+    var expected = value + value;
+    var result = calculator.add(value, value);
 
     Assertions.assertEquals(expected, result);
   }
