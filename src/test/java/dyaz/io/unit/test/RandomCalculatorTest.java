8a43a46b68f454f1ecb37d7652fe21a417699fea
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-01-22 19:51:34 +0700

    Inheritance in test

--- src/test/java/dyaz/io/unit/test/RandomCalculatorTest.java
+++ src/test/java/dyaz/io/unit/test/RandomCalculatorTest.java
@@ -1,20 +1,11 @@
 package dyaz.io.unit.test;
 
-import dyaz.io.unit.test.resolver.RandomParameterResolver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.Extensions;
 
 import java.util.Random;
 
-@Extensions({
-        @ExtendWith(RandomParameterResolver.class)
-})
-
-public class RandomCalculatorTest {
-
-  private Calculator calculator = new Calculator();
+public class RandomCalculatorTest extends AbstractCalculatorTest{
 
   @Test
   void testRandom(Random random) {
